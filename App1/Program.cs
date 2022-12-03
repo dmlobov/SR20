14fb8a075a72baaae2ba2f668ff76f7bb5729a65
dmlob <dmlob@DESKTOP-0VEH751>
2022-12-03 14:08:52 +0300

    Добавьте файлы проекта.

---
+++ App1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = CircumFerence;
+            double l = myDelegate(r);
+            Console.WriteLine(l);
+            myDelegate = CircumArea;
+            double s = myDelegate(r);
+            Console.WriteLine(s);
+            myDelegate = BallVolume;
+            double v = myDelegate(r);
+            Console.WriteLine(v);
+            Console.ReadKey();
+        }
+        static double CircumFerence(double r) => (2 * Math.PI * r);
+        static double CircumArea(double r) => (Math.PI * r*r);
+        static double BallVolume(double r) => (4.0/3*Math.PI * r*r*r);
+    }
+}
---
+++ App2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = CircumFerence;
+            myDelegate += CircumArea;
+            myDelegate += BallVolume;
+            myDelegate(r);
+            Console.ReadKey();
+        }
+        static double CircumFerence(double r)
+        {
+            double rez = 2 * Math.PI * r;
+            Console.WriteLine(rez);
+            return rez;
+        }
+        static double CircumArea(double r)
+        {
+            double rez = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine(rez);
+            return rez;
+        }
+        static double BallVolume(double r)
+        {
+            double rez = 4.0 / 3 * Math.PI *Math.Pow(r, 3);
+            Console.WriteLine(rez);
+            return rez;
+        }       
+    }
+}
